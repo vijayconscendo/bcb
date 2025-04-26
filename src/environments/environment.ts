af7fbcbc74b576d4fe5395cd70f2f1b2c95f0c25
ramya624 <31985676+ramya624@users.noreply.github.com>
2025-04-26 16:42:54 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,4 @@
 export const environment = {
     production: false,
-    auth: {
-      client_id: 'faad9acc-584a-493d-8c8a-d4b3d68f269c',
-      redirect_uri: 'https://localhost:8080',
-      grant_type:'client_credentials',
-      code_verifier:'iE8a__NHZK1Tf0aCCP_GlzPzZBVhgZ75Ut9qFhfjvcWTuu36Kl-oEALzcVBeNgcqAOJ3doOkP6IAKthJjRdUo8Ax_RwffpFhHiL7sgp6nD2cMjc9kp54pMoTYtEdOgtz',
-      client_secret: 'tNyW72w10WKh2yRXRaKMvYnO9kG7NORKWR5Vjq4wwRpbIA4iJzSIsA1oNKX7ciKt',
-      response_type:'code',
-      scope: 'openid email profile',
-      code_challenge:'xLlqIXgoZfiAwwsDUyE15sM3yN0iJLXgPA1ae32lMxc',
-      code_challenge_method:'S256',
-      nonce:'ad8c7dea-7d3d-49dc-8f0a-5e0cec70c389',
-      state:'ad8c7dea-7d3d-49dc-8f0a-5e0cec70c389'
-    }
+  
   };
