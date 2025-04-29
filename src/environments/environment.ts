ea66f61b6dace35b8af33a711a538f94f1654e03
vijayconscendo <vijayconscendo@gmail.com>
2025-04-29 11:27:32 +0530

    auth config updatedg

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,23 @@
+// export const environment = {
+//   production: false,
+//   auth: {
+//     client_id: 'faad9acc-584a-493d-8c8a-d4b3d68f269c',
+//     redirect_uri: 'https://localhost:8080',
+//     grant_type: 'client_credentials',
+//     code_verifier:
+//       'iE8a__NHZK1Tf0aCCP_GlzPzZBVhgZ75Ut9qFhfjvcWTuu36Kl-oEALzcVBeNgcqAOJ3doOkP6IAKthJjRdUo8Ax_RwffpFhHiL7sgp6nD2cMjc9kp54pMoTYtEdOgtz',
+//     client_secret:
+//       'tNyW72w10WKh2yRXRaKMvYnO9kG7NORKWR5Vjq4wwRpbIA4iJzSIsA1oNKX7ciKt',
+//     response_type: 'code',
+//     scope: 'openid email profile',
+//     code_challenge: 'xLlqIXgoZfiAwwsDUyE15sM3yN0iJLXgPA1ae32lMxc',
+//     code_challenge_method: 'S256',
+//     nonce: 'ad8c7dea-7d3d-49dc-8f0a-5e0cec70c389',
+//     state: 'ad8c7dea-7d3d-49dc-8f0a-5e0cec70c389',
+//   },
+// };
+
+
 export interface AuthConfig {
   client_id: string;
   redirect_uri: string;
@@ -12,7 +32,7 @@ export interface AuthConfig {
   state: string;
 }
 
-export const environment = {
+export const environment: { production: boolean; auth: AuthConfig } = {
   production: false,
   auth: {
     client_id: 'faad9acc-584a-493d-8c8a-d4b3d68f269c',
@@ -28,5 +48,6 @@ export const environment = {
     code_challenge_method: 'S256',
     nonce: 'ad8c7dea-7d3d-49dc-8f0a-5e0cec70c389',
     state: 'ad8c7dea-7d3d-49dc-8f0a-5e0cec70c389',
-  } as AuthConfig, // 👈 Type assertion here
+  },
 };
+
