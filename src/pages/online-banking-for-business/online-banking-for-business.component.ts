963d2f8fb5b16a962e42a3566e4998e116b851df
vijayconscendo <vijayconscendo@gmail.com>
2025-05-14 00:04:46 +0530

    changes

--- src/pages/online-banking-for-business/online-banking-for-business.component.ts
+++ src/pages/online-banking-for-business/online-banking-for-business.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { OfferSliderComponent } from '../../components/tradesuite-view/offer-slider/offer-slider.component';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { FaqComponent } from '../../components/business-banking/faq/faq.component';
 
 @Component({
   selector: 'app-online-banking-for-business',
-  imports: [CommonModule, OfferSliderComponent],
+  imports: [CommonModule, OfferSliderComponent, FaqComponent],
   templateUrl: './online-banking-for-business.component.html',
   styleUrl: './online-banking-for-business.component.scss'
 })
