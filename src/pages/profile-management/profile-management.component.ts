04e1763a8e27792b422b29c7dc54d4b39ccac8cb
vijayconscendo <vijayconscendo@gmail.com>
2025-05-14 16:36:59 +0530

    profile-manage

--- src/pages/profile-management/profile-management.component.ts
+++ src/pages/profile-management/profile-management.component.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BusinessDetailsComponent } from '../../components/profile-management-view/business-details/business-details.component';
+import { NotificationsComponent } from '../../components/profile-management-view/notifications/notifications.component';
+import { DeviceManagementComponent } from '../../components/profile-management-view/device-management/device-management.component';
+import { ConsentsComponent } from '../../components/profile-management-view/consents/consents.component';
+
 
 interface Tab {
   id: string;
@@ -12,7 +16,7 @@ interface Tab {
 
 @Component({
   selector: 'app-profile-management',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, BusinessDetailsComponent, NotificationsComponent, DeviceManagementComponent, ConsentsComponent],
   templateUrl: './profile-management.component.html',
   styleUrl: './profile-management.component.scss'
 })
